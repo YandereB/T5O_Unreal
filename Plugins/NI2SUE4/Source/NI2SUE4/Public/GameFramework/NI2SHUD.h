9bb8e2a21fdc7eeced48a507c269c2f7d6aceebe
Berta Dolcet <berta.dolcet@outlook.com>
2020-01-18 18:28:52 +0100

    All base gameplay classes

---
+++ Plugins/NI2SUE4/Source/NI2SUE4/Public/GameFramework/NI2SHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "NI2SHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NI2SUE4_API ANI2SHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
